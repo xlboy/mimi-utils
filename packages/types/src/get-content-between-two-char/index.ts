e73d8933a3430b03e4aab14d1247e56cb67f59ec
xlboy <xlboy88888@foxmail.com>
2022-05-31 08:42:45 +0800

    perf(types): 优化 `get-content-between-two-char` 注释

--- packages/types/src/get-content-between-two-char/index.ts
+++ packages/types/src/get-content-between-two-char/index.ts
@@ -1,4 +1,42 @@
-import type { A, S, N } from "ts-toolbelt";
+import type { N, S } from "ts-toolbelt";
+
+/**
+  获取两个字符之间的内容
+  
+  例如取出 `{` 与 `}` 字符之间内容，`Hello, {name}`，即 `name`
+
+  ---
+  
+  场景：取出字符中的占位符（i18n-key、接口地址等等）
+  
+  @param Str 字符串源
+  @param StartChar 需要作为获取条件的第一个字符
+  @param EndChar 需要作为获取条件的第二个字符
+  @example
+  type Str = `hello, my name is {name}, are you from {country}?`;
+
+  type Test1 = GetContentBetweenTwoChar<Str, "{", "}">;
+  type Test2 = GetContentBetweenTwoChar<Str, "{{", "}">;
+  type Test3 = GetContentBetweenTwoChar<Str, "{", "}}">;
+  type Test4 = GetContentBetweenTwoChar<Str, "", "">;
+  type Test5 = GetContentBetweenTwoChar<Str, ".", "">;
+
+  type cases = [
+    Expect<Equal<Test1, "name" | "country">>,
+    Expect<Equal<Test2, "The length of `StartChar` cannot be greater than 1">>,
+    Expect<Equal<Test3, "The length of `EndChar` cannot be greater than 1">>,
+    Expect<Equal<Test4, "`StartChar` cannot be empty">>,
+    Expect<Equal<Test5, "`EndChar` cannot be empty">>,
+  ];
+
+ */
+export type GetContentBetweenTwoChar<
+  Str extends string,
+  StartChar extends string,
+  EndChar extends string
+> = [S.Length<StartChar>, S.Length<EndChar>] extends [1, 1]
+  ? _Get<Str, StartChar, EndChar>
+  : _Warn<S.Length<StartChar>, S.Length<EndChar>>;
 
 type _Get<
   Str extends string,
@@ -43,35 +81,3 @@ type _Warn<
   : N.Greater<ECharLength, 1> extends True
   ? "The length of `EndChar` cannot be greater than 1"
   : never;
-
-/**
-  获取两个字符之间的内容。例如取出 {与} 字符之间内容： `Hello, {name}`，即 name
-  
-  @param Str 字符串源
-  @param StartChar 需要作为获取条件的第一个字符
-  @param EndChar 需要作为获取条件的第二个字符
-  @example
-  type Str = `hello, my name is {name}, are you from {country}?`;
-
-  type Test1 = GetContentBetweenTwoChar<Str, "{", "}">;
-  type Test2 = GetContentBetweenTwoChar<Str, "{{", "}">;
-  type Test3 = GetContentBetweenTwoChar<Str, "{", "}}">;
-  type Test4 = GetContentBetweenTwoChar<Str, "", "">;
-  type Test5 = GetContentBetweenTwoChar<Str, ".", "">;
-
-  type cases = [
-    Expect<Equal<Test1, "name" | "country">>,
-    Expect<Equal<Test2, "The length of `StartChar` cannot be greater than 1">>,
-    Expect<Equal<Test3, "The length of `EndChar` cannot be greater than 1">>,
-    Expect<Equal<Test4, "`StartChar` cannot be empty">>,
-    Expect<Equal<Test5, "`EndChar` cannot be empty">>,
-  ];
-
- */
-export type GetContentBetweenTwoChar<
-  Str extends string,
-  StartChar extends string,
-  EndChar extends string
-> = [S.Length<StartChar>, S.Length<EndChar>] extends [1, 1]
-  ? _Get<Str, StartChar, EndChar>
-  : _Warn<S.Length<StartChar>, S.Length<EndChar>>;
