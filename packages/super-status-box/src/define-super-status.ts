d7f56f8ba033000ab63ca432f1491aa32e9bac80
xlboy <xlboy88888@foxmail.com>
2022-06-05 14:59:16 +0800

    types(super-status-box): 完善 `defineSuperStatus` 的类型验证
    `alias` 不可重复

--- packages/super-status-box/src/define-super-status.ts
+++ packages/super-status-box/src/define-super-status.ts
@@ -1,5 +1,14 @@
 import type { SuperStatus } from './types';
+import type { L, U } from 'ts-toolbelt';
 
-export function defineSuperStatus<T extends SuperStatus>(superStatus: ReadonlyArray<T>) {
+type VerifyStatusAliasRepeat<T extends ReadonlyArray<SuperStatus>> = L.Length<
+  U.ListOf<T[number]['alias']>
+> extends L.Length<T>
+  ? true
+  : false;
+
+export function defineSuperStatus<T extends ReadonlyArray<SuperStatus>>(
+  superStatus: T extends (VerifyStatusAliasRepeat<T> extends true ? T : false) ? T : '别名不可重复哦'
+) {
   return superStatus;
 }
