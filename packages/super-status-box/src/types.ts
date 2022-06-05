ba3610f82adb089d04ceb130f705143d4ad424ee
xlboy <xlboy88888@foxmail.com>
2022-06-05 14:56:20 +0800

    types(super-status-box): 调整 `SuperStatus` 的 `alias` 为必填

--- packages/super-status-box/src/types.ts
+++ packages/super-status-box/src/types.ts
@@ -1,9 +1,4 @@
-export type SuperStatus = { key: string | number; alias?: string; unifyLabel: string };
-
-// export type InferTypeOfSuperStatus<T extends Readonly<SuperStatus>> = {
-//   keys: keyof T;
-//   aliases: T[keyof T]['alias'];
-// };
+export type SuperStatus = { key: string | number; alias: string; unifyLabel: string };
 
 export type ParamsOfSuperStatusToOptions<T = any, D = any> = {
   returnAlias?: boolean;
